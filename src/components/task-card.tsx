ee3a9aa239fa8f46d983fafdc594ce2b88511246
Imam Alfarizi <imam.alfarizi.777@gmail.com>
2025-08-22 13:44:49 +0700

    priority dots

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -5,7 +5,20 @@ export function TaskCard({ task }: { task: Task }) {
     <div className="m-2 w-56 rounded-lg border border-gray-300 bg-gray-50 px-2">
       <div className="py-2">{task.title}</div>
       <div className="flex justify-between gap-4 py-2 text-sm text-gray-500">
-        <div className="">{task.id}</div> <div className="">{task.points}</div>
+        <div className="flex items-center gap-2">
+          <div>{task.id}</div>
+          <div>{task.priority}</div>
+          {task.priority === "HIGH" && (
+            <div className="h-2 w-2 rounded-full bg-red-500" />
+          )}
+          {task.priority === "MEDIUM" && (
+            <div className="h-2 w-2 rounded-full bg-yellow-500" />
+          )}
+          {task.priority === "LOW" && (
+            <div className="h-2 w-2 rounded-full bg-green-500" />
+          )}
+        </div>
+        <div className="">{task.points}</div>
       </div>
     </div>
   );
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -1,37 +1,45 @@
-export type Status = 'TODO' | 'IN_PROGRESS' | 'DONE';
+export type Status = "TODO" | "IN_PROGRESS" | "DONE";
+
+export type Priority = "LOW" | "MEDIUM" | "HIGH";
 
 export type Task = {
   title: string;
   id: string;
   status: Status;
+  priority: Priority;
   points?: number;
 };
 
-export const statuses: Status[] = ['TODO', 'IN_PROGRESS', 'DONE'];
+export const statuses: Status[] = ["TODO", "IN_PROGRESS", "DONE"];
+export const priorities: Priority[] = ["LOW", "MEDIUM", "HIGH"];
 
 export const tasks: Task[] = [
   {
-    title: 'Do Market Research',
-    id: 'BUS-1',
-    status: 'TODO',
+    title: "Do Market Research",
+    id: "BUS-1",
+    status: "TODO",
+    priority: "HIGH",
     points: 5,
   },
   {
-    title: 'Cooking Some Food',
-    id: 'BUS-1',
-    status: 'TODO',
+    title: "Cooking Some Food",
+    id: "BUS-1",
+    status: "TODO",
+    priority: "HIGH",
     points: 5,
   },
   {
-    title: 'Study at the Library',
-    id: 'BUS-2',
-    status: 'IN_PROGRESS',
+    title: "Study at the Library",
+    id: "BUS-2",
+    status: "IN_PROGRESS",
+    priority: "MEDIUM",
     points: 7,
   },
   {
-    title: 'Playing Football',
-    id: 'BUS-3',
-    status: 'DONE',
+    title: "Playing Football",
+    id: "BUS-3",
+    status: "DONE",
+    priority: "LOW",
     points: 9,
   },
 ];
