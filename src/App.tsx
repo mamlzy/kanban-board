23ab1dce15d38b1e6dcce5b9a2b3427546e63ff7
Imam Alfarizi <imam.alfarizi.777@gmail.com>
2025-08-22 14:07:56 +0700

    update task points

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { statuses, tasks } from "./utils/data-tasks";
+import { statuses, tasks as initialTasks, type Task } from "./utils/data-tasks";
 import { TaskCard } from "./components/task-card";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status);
 
@@ -11,15 +14,24 @@ function App() {
     };
   });
 
+  const updateTaskPoints = (task: Task, points: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((t) => (t.id === task.id ? { ...t, points } : t)),
+    );
+  };
+
   return (
     <div className="flex divide-x divide-gray-200">
       {columns.map((column) => (
         <div className="">
-          <h2 className="p-2 text-3xl font-bold text-gray-500 capitalize">
-            {column.status}
-          </h2>
+          <div className="flex items-center justify-between p-2 font-bold text-gray-500">
+            <h2 className="text-3xl capitalize">{column.status}</h2>
+            <span className="text-lg">
+              {column.tasks.reduce((acc, task) => acc + (task.points || 0), 0)}
+            </span>
+          </div>
           {column.tasks.map((task) => (
-            <TaskCard task={task} />
+            <TaskCard task={task} updateTaskPoints={updateTaskPoints} />
           ))}
         </div>
       ))}
--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,13 +1,22 @@
 import type { Task } from "../utils/data-tasks";
+import { MinusIcon, PlusIcon } from "lucide-react";
 
-export function TaskCard({ task }: { task: Task }) {
+export function TaskCard({
+  task,
+  updateTaskPoints,
+}: {
+  task: Task;
+  updateTaskPoints: (task: Task, points: number) => void;
+}) {
+  const points = task.points || 0;
+
   return (
-    <div className="m-2 w-56 rounded-lg border border-gray-300 bg-gray-50 px-2">
+    <div className="m-2 w-60 rounded-lg border border-gray-300 bg-gray-50 px-2">
       <div className="py-2">{task.title}</div>
       <div className="flex justify-between gap-4 py-2 text-sm text-gray-500">
         <div className="flex items-center gap-2">
-          <div>{task.id}</div>
-          <div>{task.priority}</div>
+          <span>{task.id}</span>
+          <span>{task.priority}</span>
           {task.priority === "HIGH" && (
             <div className="h-2 w-2 rounded-full bg-red-500" />
           )}
@@ -18,7 +27,15 @@ export function TaskCard({ task }: { task: Task }) {
             <div className="h-2 w-2 rounded-full bg-green-500" />
           )}
         </div>
-        <div className="">{task.points}</div>
+        <div className="flex items-center gap-1">
+          <button onClick={() => updateTaskPoints(task, points - 1)}>
+            <MinusIcon className="size-3" />
+          </button>
+          <span className="font-bold">{points}</span>
+          <button onClick={() => updateTaskPoints(task, points + 1)}>
+            <PlusIcon className="size-3" />
+          </button>
+        </div>
       </div>
     </div>
   );
